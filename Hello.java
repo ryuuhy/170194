8e8a442f77f9e906b140f53358be47dd193b9df0
ryuuhy <33193787+ryuuhy@users.noreply.github.com>
2017-11-24 11:47:18 +0900

    upload

---
+++ Hello.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Container;
+import java.awt.BorderLayout;
+
+public class Hello extends JFrame{
+
+	Hello(String title){
+		setTitle(title);
+		setSize(200,200);
+		setLocation(300,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JButton btn = new JButton("Click ME!");
+
+		JPanel panel = new JPanel();
+		panel.add(btn);
+
+		Container cp = getContentPane();
+		cp.add(panel,BorderLayout.CENTER);
+	}
+
+	public static void main(String[] args){
+		Hello hello = new Hello("aloha");
+		hello.setVisible(true);
+	}
+}
